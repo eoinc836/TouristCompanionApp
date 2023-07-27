f202d91c29f2d97c9adfc8099bcd45757ca07988
samCorr4 <123460550+samCorr4@users.noreply.github.com>
2023-07-27 23:50:22 +0100

    Edits to various files, Guide still incomplete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,28 @@
+import React, { useEffect, useState } from "react";
 import { useRoutes } from "react-router-dom";
 import Routes from "./router/indexRouter";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import StartUpGuide from "./components/StartUpGuide";
 
 function App() {
+  const [showGuide, setShowGuide] = useState(false);
+
+  useEffect(() => {
+    const hasSeenGuide = localStorage.getItem("hasSeenGuide");
+    if (!hasSeenGuide) {
+      setShowGuide(true);
+      localStorage.setItem("hasSeenGuide", "true");
+    }
+  }, []);
+
   return (
     <div>
       <Header></Header>
       {useRoutes(Routes)}
       <Footer></Footer>
+
+      {showGuide && <StartUpGuide />} 
     </div>
   );
 }
