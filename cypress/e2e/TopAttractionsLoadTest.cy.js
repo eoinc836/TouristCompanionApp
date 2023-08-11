a42e7c6a73018042d646a2446b70c0c2ba41c181
samCorr4 <123460550+samCorr4@users.noreply.github.com>
2023-08-11 17:48:11 +0100

    Updated cypress tests

--- cypress/e2e/TopAttractionsLoadTest.cy.js
+++ cypress/e2e/TopAttractionsLoadTest.cy.js
@@ -1,5 +1,5 @@
 describe('Top Attractions Load Test', () => {
   it('Visits Top Attractions Page', () => {
-    cy.visit('https://localhost3000/TopAttractions')
+    cy.visit('https://localhost:3000/topattractions')
   })
 })
